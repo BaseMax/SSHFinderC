d2bcdee16451f7d834ae1f1335acc7ff80274e5b
MaxianEdison <MaximilianEdison@gmail.com>
2023-07-01 17:48:13 +0330

    Back to work

--- src/sshfinder.c
+++ src/sshfinder.c
@@ -1,57 +1,74 @@
+#include "sshfinder.h"
+#include <getopt.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <unistd.h>
-#include <getopt.h>
+#include <string.h>
 
 
-// Show help message
-void show_help(void) {
-	printf("Help message:\n");
-	printf("  -r,  --range: Specifies the IP range or local network to scan. "
-		   "The format can be a single IP address,\n\t\t"
-		   "a range using CIDR notation (e.g., 192.168.1.0/24)," 
-		   "or a combination of both (e.g., 192.168.1.0/24,192.168.2.0/24).\n\n");
-	printf("  -p,  --port: Specifies the port number"
-		   "to scan for SSH services (default is port 22).\n\n");
-	printf("  -t,  --threads: Specifies the number of concurrent threads to"
-		   " use for scanning (default is 10).\n\n");
-	printf("  -h,  --help: Displays the help message and usage information.\n\n");
+#define MAX_RANGE_SIZE 100
+
+int
+main(int argc, char *argv[])
+{
+    char range[MAX_RANGE_SIZE];
+    int port = 22;
+    int thread = 10;
+    int help_flag = 0;
+
+    static struct option long_options[] = {
+        {"help"  ,   no_argument,       0, 'h'},
+        {"range" ,   required_argument, 0, 'r'},
+        {"port"  ,   required_argument, 0, 'p'},
+        {"thread",   required_argument, 0, 't'},
+        {0       ,   0                , 0,   0}
+    };
+    
+    int option;
+    while ((option = getopt_long(argc, argv, "hr:p:t:", long_options, NULL)) != -1) {
+        if (option == 'h') {
+            help_flag = 1;
+            break;
+        }
+
+        switch (option) {
+            case 'r':
+                strncpy(range, optarg, MAX_RANGE_SIZE);
+                printf("Range = %s\n", range);
+                break;
+            case 'p':
+                port = atoi(optarg);
+                printf("Port : %d\n", port);
+                break;
+            case 't':
+                thread = atoi(optarg);
+                printf("Thread = %d\n", thread);
+                break;
+            default:
+                fprintf(stderr,
+                        "Option %s not provided.\n",
+                        argv[1]
+                       );
+                exit(EXIT_FAILURE);
+        }
+    }
+
+    if (help_flag) {
+        usage();
+        return EXIT_SUCCESS;
+    }
+
+    return EXIT_SUCCESS;
 }
 
-int main(int argc, char *argv[]) {
-	int option;
-	int option_index = 0;
-
-	static struct option long_options[] = {
-		{"range", required_argument, 0, 'r'},
-		{"port", required_argument, 0, 'p'},
-		{"threads", required_argument, 0, 't'},
-		{"help", no_argument, 0, 'h'},
-		{0, 0, 0, 0}
-	};
-
-	while((option = getopt_long(argc, argv, "r:p:t:h", long_options, &option_index)) != -1) {
-		switch(option) {
-			case 'r':
-
-				break;
-			case 'p':
-
-				break;
-
-			case 't':
-
-				break;
-
-			case 'h':
-				show_help();
-				break;
-			case '?':
-				// Handle unrecognized options or missing arguments.
-				exit(1);
-			default:
-				// Handle other cases
-				break;
-		}
-	}
+static void usage(void)
+{
+    printf(
+"Usage information:\n"
+"\t-h     --help          Display the help message and usage information\n"
+"\t-r     --range         The ip range to scan (ex. 192.168.0.1/24,192.168.2.0/24)\n"
+"\t-p     --port          The port number to scan for SSH services (default is 22)\n"
+"\t-t     --threads       The number of concurrent therads to use for scanning (default is 10)\n"
+"Authors: Maximilian Edison <maximilianedison@gmail.com>\n"
+"         BaseMax           <basemaxcode@gmail.com>\n"
+    );
 }
---
+++ src/sshfinder.h
@@ -0,0 +1,7 @@
+#ifndef SSHFINDER_H
+#define SSHFINDER_H
+
+// Print help message
+static void usage(void);
+
+#endif // SSHFINDER_H
